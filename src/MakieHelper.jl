8811b4afc1f97a2031f50fc841f7248801dbd90f
LudwigBoess <ludwig.boess@outlook.de>
2026-01-04 21:09:27 +0000

    added symlog10

--- src/MakieHelper.jl
+++ src/MakieHelper.jl
@@ -4,6 +4,7 @@ module MakieHelper
 
     include("binning.jl")
     include("plot_styling.jl")
+    include("symlog10.jl")
 
     export get_theme,
             set_dark_theme!,
@@ -15,6 +16,7 @@ module MakieHelper
             bin_2D, bin_2D!,
             bin_2D_quantity!,
             bin_2D_log, bin_2D_log!,
-            bin_2D_quantity_log!
+            bin_2D_quantity_log!,
+            Symlog10
 
 end
